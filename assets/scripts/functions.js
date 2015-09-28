373d9ad6a4dfc594d56b13468ce02b9d6d7865e6
Hudson Nicoletti <hudson19962009@gmail.com>
2015-09-28 14:15:07 -0300

    Refactoring

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,220 +1,187 @@
-$(function() {
+(function($){
     
-    $.scrollIt();
-    
-     function stickyHeader(offset)
-    {
+    $(document).ready(function(){
         
-        if( offset > 125 )
+        var $window     = $(window),
+            $homeSlider = $('.slides'),
+            $hero       = $("section.hero"),
+            $logo       = $('.logo-container'),
+            $menu       = $('.menu-container'),
+            $menuBtn    = $('.menu-toggle'),
+            $menuMobile = $('.menu-container.mobile'),
+            $search     = $('.search-container'),
+            $searchBtn  = $('.search-toggle'),
+            $scrollBtn  = $(".scroll-top"),
+            $map        = $("#map-coords"),
+            $mapArea    = $("#map-coords area"),
+            $mapInfo    = $(".map-info"),
+            SSConfig    = {
+                hashchange: false,
+                slide_speed: 5000,
+                pagination: false,
+                play: 5000
+            };
+        
+        function stickyHeader(offset)
         {
-            $("header").addClass("sticky");
+            if( offset > 125 )
+            {
+                $("header").addClass("sticky");
+            }
+            if( offset < 125 )
+            {
+                $("header").removeClass("sticky");
+            }
         }
-        if( offset < 125 )
+        
+        function heroBackground(offset)
         {
-            $("header").removeClass("sticky");
+            if (offset <= $hero.height() )
+            {
+                $hero.css({
+                    'background-position' : " 0px  -"+ (offset * 8 ) / 5 +"px "
+                });
+            }
         }
-    }
-    
-    function scrollTop(offset)
-    {
         
-        var slider = $(".slides"),
-            topbtn = $(".scroll-top"),
-            toggleH = (slider.height()/4);
-        
-        if( offset > toggleH )
-        {            
-            topbtn.css({
-                'opacity' : 1
-            });            
-        }
-        if( offset < toggleH )
+        function scrollTopVisibility(offset)
         {
-            topbtn.css({
-                'opacity' : 0
-            });         
+            var toggleH = ($window.height()/4);
+
+            if( offset > toggleH )
+            {            
+                $scrollBtn.css({
+                    'opacity' : 1
+                });            
+            }
+            if( offset < toggleH )
+            {
+                $scrollBtn.css({
+                    'opacity' : 0
+                });         
+            }
         }
         
-    }
-    
-    function heroBackground(offset)
-    {
-        var hero = $("section.hero");
+        function scrollTop()
+        {
+            $scrollBtn.on("click", function(){
+                $('html, body').animate({
+                    scrollTop : 0
+                },800);
+
+                return false;
+            })
+        }
         
-        if (offset <= hero.height() )
+        function searchToggle()
+        {
+            $searchBtn.on('click', function(){
+
+                if( $search.hasClass('active') )
+                {
+
+                    $menu.css({
+                    'opacity' : 1
+                    });
+
+                    $logo.css({
+                        'opacity' : 1
+                    });
+
+                    $search.css({
+                        'opacity' : 0,
+                        'pointer-events' : 'none'
+                    }).removeClass('active');
+
+                }
+                else
+                {
+
+                    $menu.css({
+                    'opacity' : 0
+                    });
+
+                    $logo.css({
+                        'opacity' : 0
+                    });
+
+                    $search.css({
+                        'opacity' : 1,
+                        'pointer-events' : 'all'
+                    }).addClass('active');
+
+                }
+
+            }); 
+        }
+        
+        function mobileMenu()
+        {
+            $menuBtn.on('click', function(){
+                $menuMobile.toggle();
+            }); 
+        }
+        
+        function mapInteractive()
         {
             
-            $("section.hero").css({
-                'background-position' : " 0px  -"+ (offset * 8 ) / 5 +"px "
+            $mapArea.on("mouseover", function(){
+                
+                var i       = $(this).data("info"),
+                stats_01    = $("[data-stats='1']"),
+                stats_02    = $("[data-stats='2']"),
+                stats_03    = $("[data-stats='3']");
+
+            var info = [
+
+                { title : "Paraná"             , sub : "Londrina, Cambé, Rolândia, Arapongas, Guaratuba, Ortigueira" , _1 : 24 , _2 : 02 , _3: 05 },
+                { title : "SÃO PAULO"          , sub : "Aparecida, São Paulo, Campinas, Araçariguama, Serra Negra" , _1 : 05 , _2 : 00 , _3: 06 },
+                { title : "RIO DE JANEIRO"     , sub : "Rio de Janeiro" , _1 : 00 , _2 : 00 , _3: 02 },
+                { title : "MINAS GERAIS"       , sub : "Uberaba" , _1 : 00 , _2 : 01 , _3: 00 },
+                { title : "RIO GRANDE DO SUL"  , sub : "Canoas" , _1 : 00 , _2 : 00 , _3: 01 },
+                { title : "SANTA CATARINA"     , sub : "Lages" , _1 : 00 , _2 : 00 , _3: 01 },
+                { title : "BAHIA"              , sub : "Lauro de Freitas" , _1 : 00 , _2 : 00 , _3: 01 },
+                { title : "PARAGUAI"           , sub : "Salto del Guairá" , _1 : 00 , _2 : 00 , _3: 01 }
+
+            ];
+
+            $mapInfo.find("span.title").text( info[i].title );
+            $mapInfo.find("span.sub").text( info[i].sub );
+            stats_01.find("span").text( info[i]._1 );
+            stats_02.find("span").text( info[i]._2 );
+            stats_03.find("span").text( info[i]._3 );
+                
             });
-
-        }
-        
-    }
-    
-    $('.slides').superslides({
-        hashchange: false,
-        slide_speed: 5000,
-        pagination: false,
-        play: 5000
-    });
-    
-    //  SLIDER NAVIGATION
-    $('nav.slides-navigation a').on('click', function(){
-        
-        if( $(this).hasClass('prev') )
-        {
-            $('.slides').superslides('animate', 'prev');
-        }
-        if( $(this).hasClass('next') )
-        {
-            $('.slides').superslides('animate', 'next');
-        }
-        
-    });
-    
-    $('.search-toggle').on('click', function(){
-        
-        var $this = this,
-            $menu = $('.menu-container'),
-            $logo = $('.logo-container'),
-            $search = $('.search-container');
-        
-        if( $search.hasClass('active') )
-        {
-            
-            $menu.css({
-            'opacity' : 1
-            });
-
-            $logo.css({
-                'opacity' : 1
-            });
-
-            $search.css({
-                'opacity' : 0,
-                'pointer-events' : 'none'
-            }).removeClass('active');
             
         }
-        else
-        {
-            
-            $menu.css({
-            'opacity' : 0
-            });
 
-            $logo.css({
-                'opacity' : 0
-            });
-
-            $search.css({
-                'opacity' : 1,
-                'pointer-events' : 'all'
-            }).addClass('active');
-            
-        }
-        
-    }); 
+        $(window).scroll(function(){
     
-    $('.menu-toggle').on('click', function(){
+            var offset = $(window).scrollTop();
         
-        var $this = this,
-            $menu = $('.menu-container.mobile');
-        
-        $menu.toggle();
-        
-    }); 
-    
-    $("#carousel").owlCarousel({
- 
-        autoPlay: 3000,
-        stopOnHover : true,
-        items : 3,
-        responsiveBaseWidth:$("#carousel")
-
-    });
-    
-    $("#carousel-next").on("click", function(){
-        
-        var owl = $("#carousel").data('owlCarousel');
-        owl.next();
-        
-    });
-    
-    $("#contact").submit(function(){
-        
-        $(this).find("input").each(function(){
-            
-            
+            stickyHeader(offset);
+            heroBackground(offset);
+            scrollTopVisibility(offset);
             
         });
         
-        return false;
-    });
-    
-    $("#contact input, #contact textarea").focus(function(){
-        
-        if( $(this).hasClass("error") ){
-            $(this).removeClass("error");
-            $(this).siblings().css({
-                'opacity' : 0
-            });
+        if( $homeSlider.length )
+        {   
+            $homeSlider.superslides(SSConfig)
         }
         
+        if( $map.length )
+        {
+            $("#map-coords area[data-info='0']").mouseover();
+            mapInteractive();
+        }        
+        
+        $.scrollSpeed(100, 800);
+        $.scrollIt();
+        
+        searchToggle();
+        mobileMenu();
+        scrollTop();
     });
     
-    $(".scroll-top").on("click", function(){
-       
-        $('html, body').animate({
-            scrollTop : 0
-        },800);
-        
-        return false;
-    });
-    
-    $("#map-coords area").hover(function(){
-        
-        var i           = $(this).data("info"),
-            mapinfo     = $(".map-info"),
-            stats_01    = $("[data-stats='1']"),
-            stats_02    = $("[data-stats='2']"),
-            stats_03    = $("[data-stats='3']");
-        
-        var info = [
-            
-            { title : "Paraná"             , sub : "Londrina, Cambé, Rolândia, Arapongas, Guaratuba, Ortigueira" , _1 : 24 , _2 : 02 , _3: 05 },
-            { title : "SÃO PAULO"          , sub : "Aparecida, São Paulo, Campinas, Araçariguama, Serra Negra" , _1 : 05 , _2 : 00 , _3: 06 },
-            { title : "RIO DE JANEIRO"     , sub : "Rio de Janeiro" , _1 : 00 , _2 : 00 , _3: 02 },
-            { title : "MINAS GERAIS"       , sub : "Uberaba" , _1 : 00 , _2 : 01 , _3: 00 },
-            { title : "RIO GRANDE DO SUL"  , sub : "Canoas" , _1 : 00 , _2 : 00 , _3: 01 },
-            { title : "SANTA CATARINA"     , sub : "Lages" , _1 : 00 , _2 : 00 , _3: 01 },
-            { title : "BAHIA"              , sub : "Lauro de Freitas" , _1 : 00 , _2 : 00 , _3: 01 },
-            { title : "PARAGUAI"           , sub : "Salto del Guairá" , _1 : 00 , _2 : 00 , _3: 01 }
-            
-        ];
-        
-        mapinfo.find("span.title").text( info[i].title );
-        mapinfo.find("span.sub").text( info[i].sub );
-        stats_01.find("span").text( info[i]._1 );
-        stats_02.find("span").text( info[i]._2 );
-        stats_03.find("span").text( info[i]._3 );
-        
-    },function(){});
-    
-    if( $("#map-coords").length )
-    {
-        $("#map-coords area[data-info='0']").mouseover();
-    }
-    
-    $(window).scroll(function(){
-    
-        var offset = $(window).scrollTop();
-        
-        heroBackground(offset);
-        stickyHeader(offset);
-        scrollTop(offset);
-        
-    }).scroll();
-    
-});
+})(jQuery)
